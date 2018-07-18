455ce9cf2beea2353d2eb858332f3321a5c1095f
Cole Druffel <Druffel12@gmail.com>
2018-07-18 14:07:00 -0700

    start commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+    public float JumpSpeed;
+    public float JumpTimer;
+
+    void start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }	
+
+	void Update ()
+    {
+        float Translation = Input.GetAxis("Vertical") * Speed;
+        float Straffe = Input.GetAxis("Horizontal") * Speed;
+        
+        Translation *= Time.deltaTime;
+        Straffe *= Time.deltaTime;
+       
+        transform.Translate(Straffe, 0, Translation);
+
+        JumpTimer -= Time.deltaTime;
+
+        if(JumpTimer > 1)
+        {
+            float up = Input.GetAxis("Jump") * JumpSpeed;
+            Jump *= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+       	
+	}
+}
